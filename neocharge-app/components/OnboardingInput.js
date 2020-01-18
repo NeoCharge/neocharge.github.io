4ddc185f145685dac1cb23f024448980fe61fcc4
caseydaly <caseydaly31@gmail.com>
2020-01-17 16:42:27 -0800

    finished UI for onboarding, almost done with connection to Lambda

--- neocharge-app/components/OnboardingInput.js
+++ neocharge-app/components/OnboardingInput.js
@@ -1,13 +1,30 @@
-import React, {useState} from 'react';
-import { View, StyleSheet, Image, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Image, Text, Button, Picker } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import ModalDropdown from 'react-native-modal-dropdown'
 
 const OnboardingInput = props => {
+
+    const timeZoneInputs = ['PST', 'MST', 'CST', 'EST'];
+    const deviceInputs = ['Vehicle', 'Washer/Dryer', 'None'];
 
     const [deviceID, setDeviceID] = useState('');
 
     const deviceInputHandler = (enteredText) => {
-      setDeviceID(enteredText);
+        setDeviceID(enteredText);
+        props.setDeviceIDHandler(enteredText);
+    };
+
+    const timeZoneHandler = (selectedVal) => {
+        props.timeZoneHandler(timeZoneInputs[selectedVal]);
+    };
+
+    const primDevHandler = (selectedVal) => {
+        props.primDevHandler(deviceInputs[selectedVal]);
+    };
+
+    const secDevHandler = (selectedVal) => {
+        props.secDevHandler(deviceInputs[selectedVal]);
     };
 
     return (
@@ -23,7 +40,21 @@ const OnboardingInput = props => {
             </View>
             <View style={styles.deviceInfoContainer}>
                 <Text style={styles.promptText}>Select Time Zone</Text>
-                
+                <View style={styles.pickerContainer}>
+                    <ModalDropdown options={timeZoneInputs} onSelect={timeZoneHandler} />
+                </View>
+            </View>
+            <View style={styles.deviceInfoContainer}>
+                <Text style={styles.promptText}>Select Primary Device</Text>
+                <View style={styles.pickerContainer}>
+                    <ModalDropdown options={deviceInputs} onSelect={primDevHandler} />
+                </View>
+            </View>
+            <View style={styles.deviceInfoContainer}>
+                <Text style={styles.promptText}>Select Secondary Device</Text>
+                <View style={styles.pickerContainer}>
+                    <ModalDropdown options={deviceInputs} onSelect={secDevHandler} />
+                </View>
             </View>
 
         </View>
@@ -34,12 +65,14 @@ const styles = StyleSheet.create({
     inputContainer: {
         justifyContent: 'center',
         alignItems: 'center',
-        padding: 20
+        paddingVertical: 20
     },
     deviceInfoContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        width: '90%'
+        alignItems: "center",
+        width: '90%',
+        padding: 10
     },
     promptText: {
         textAlign: "center",
@@ -48,6 +81,9 @@ const styles = StyleSheet.create({
     textContainer: {
         flexDirection: 'column',
         justifyContent: 'center'
+    },
+    itemStyle: {
+        fontSize: 10
     }
 });
 
--- neocharge-app/screens/SetupScreen.js
+++ neocharge-app/screens/SetupScreen.js
@@ -2,8 +2,45 @@ import React, { useState } from 'react';
 import { View, TextInput, StyleSheet, Button, Modal } from 'react-native';
 import OnboardingLogo from '../components/OnboardingLogo';
 import OnboardingInput from '../components/OnboardingInput';
+import { API } from 'aws-amplify';
 
 const SetupScreen = props => {
+
+  const [deviceID, setDeviceID] = useState('');
+  const [timeZone, setTimeZone] = useState('');
+  const [primaryDevice, setPrimaryDevice] = useState('');
+  const [secondaryDevice, setSecondaryDevice] = useState('');
+
+
+
+  const timeZoneHandler = (selectedVal) => {
+      setTimeZone(selectedVal);
+  };
+
+  const primDevHandler = (selectedVal) => {
+      setPrimaryDevice(selectedVal);
+  };
+
+  const secDevHandler = (selectedVal) => {
+      setSecondaryDevice(selectedVal);
+  };
+
+  const setDeviceIDHandler = (device) => {
+    setDeviceID(device);
+  }
+
+  async function logOnboardingInfo() {
+    console.log("timeZone: " + timeZone);
+    console.log("primaryDevice: " + primaryDevice);
+    console.log("secondaryDevice: " + secondaryDevice);
+    console.log("deviceID: " + deviceID);
+
+    let jsonObj = {"timeZone":timZone, "primaryDevice":primaryDevice, "secondaryDevice":secondaryDevice, "deviceID":deviceID};
+
+    const path = "/user"; // you can specify the path
+    const apiResponse = await API.put("LambdaProxy", path, jsonObj); //replace the API name
+    console.log(apiResponse);
+  };
 
   return (
     <View>
@@ -11,10 +48,14 @@ const SetupScreen = props => {
         <OnboardingLogo />
       </View>
       <View style={styles.infoContainer}>
-        <OnboardingInput />
+        <OnboardingInput 
+        primDevHandler={primDevHandler} 
+        timeZoneHandler={timeZoneHandler} 
+        secDevHandler={secDevHandler}
+        setDeviceIDHandler={setDeviceIDHandler}/>
       </View>
       <View style={styles.buttonContainer}>
-        <Button title="Continue" />
+        <Button title="Continue" onPress={logOnboardingInfo}/>
       </View>
     </View>
   );
