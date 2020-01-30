76716214852a3b37832ab8436ba7ece57db2808e
hkwan8 <hcekwan@gmail.com>
2020-01-30 09:41:35 -0800

    revert conflicting files

--- neocharge-app/__tests__/RateDisplay.test.js
+++ neocharge-app/__tests__/RateDisplay.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import RateDisplay from '../RateDisplay';
+import RateDisplay from '../components/RateDisplay';
 
 describe('RateDisplay', () => {
     describe('Rendering', () => {
