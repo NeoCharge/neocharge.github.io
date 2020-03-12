c85ac2d4efd95a1401b7fa67378dadc24968f1ed
hkwan8 <hcekwan@gmail.com>
2020-03-11 17:29:30 -0700

    changes made while preparing the demo

--- neocharge-app/screens/NotificationsScreen.js
+++ neocharge-app/screens/NotificationsScreen.js
@@ -3,95 +3,97 @@ import { StyleSheet, Text, View, Image, Switch, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Colors from '../assets/colors.js';
-import {API} from 'aws-amplify';
+import { API } from 'aws-amplify';
 
 class NotificationSelectionScreen extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        // Dummy email currently being used. 
-        // Needs to be changed to the email that user inputted on previous screens
-        userEmail: 'test@calpoly.edu',
-        primaryDeviceSwitch: false, 
-        secondaryDeviceSwitch: false, 
-        chargeInterruptSwitch: false 
-      }
-      this.primaryDeviceToggle = this.primaryDeviceToggle.bind(this);
-      this.secondaryDeviceToggle = this.secondaryDeviceToggle.bind(this);
-      this.chargeInterruptToggle = this.chargeInterruptToggle.bind(this);
-      this.onlogInfo = this.onlogInfo.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = {
+      // Dummy email currently being used. 
+      // Needs to be changed to the email that user inputted on previous screens
+      userEmail: 'test@calpoly.edu',
+      primaryDeviceSwitch: false,
+      secondaryDeviceSwitch: false,
+      chargeInterruptSwitch: false
     }
+    this.primaryDeviceToggle = this.primaryDeviceToggle.bind(this);
+    this.secondaryDeviceToggle = this.secondaryDeviceToggle.bind(this);
+    this.chargeInterruptToggle = this.chargeInterruptToggle.bind(this);
+    this.onlogInfo = this.onlogInfo.bind(this);
+  }
 
- 
-    primaryDeviceToggle(value) {
-      this.setState({ primaryDeviceSwitch: value });
+
+  primaryDeviceToggle(value) {
+    this.setState({ primaryDeviceSwitch: value });
+  };
+
+  secondaryDeviceToggle(value) {
+    this.setState({ secondaryDeviceSwitch: value })
+  };
+
+  chargeInterruptToggle(value) {
+    this.setState({ chargeInterruptSwitch: value })
+  };
+
+  //onValueChange of the switch this function will be called
+  async onlogInfo() {
+    console.log("User Email: " + this.state.userEmail);
+    console.log("Primary Device Switch: " + this.state.primaryDeviceSwitch);
+    console.log("Secondary Device Switch: " + this.state.secondaryDeviceSwitch);
+    console.log("Charge Interruptions Switch: " + this.state.chargeInterruptSwitch);
+
+    let requestBody = {
+      "email": this.state.userEmail,
+      "primarydevice": this.state.primaryDeviceSwitch,
+      "secondardevice": this.state.secondaryDeviceSwitch,
+      "chargeinterruptions": this.state.chargeInterruptSwitch
     };
-
-    secondaryDeviceToggle(value) { 
-      this.setState({ secondaryDeviceSwitch: value })
-    };
-
-    chargeInterruptToggle(value) { 
-      this.setState({ chargeInterruptSwitch: value })
-    };
-
-    //onValueChange of the switch this function will be called
-   async onlogInfo() { 
-      console.log("User Email: " + this.state.userEmail);
-      console.log("Primary Device Switch: " + this.state.primaryDeviceSwitch);
-      console.log("Secondary Device Switch: " + this.state.secondaryDeviceSwitch);
-      console.log("Charge Interruptions Switch: " + this.state.chargeInterruptSwitch); 
-
-      let requestBody = { "email": this.state.userEmail,
-                          "primarydevice": this.state.primaryDeviceSwitch, 
-                          "secondardevice": this.state.secondaryDeviceSwitch,
-                          "chargeinterruptions": this.state.chargeInterruptSwitch};
-      let jsonObj = {
+    let jsonObj = {
       body: requestBody
-      }
-      const path = "/settings"; // path from root of API
-      const apiResponse = await API.put("LambdaProxy", path, jsonObj); //replace the desired API name
-      console.log(apiResponse);
-    };
-  
-    render() {
-        return (
-          <View style={styles.container}>
+    }
+    const path = "/settings"; // path from root of API
+    const apiResponse = await API.put("LambdaProxy", path, jsonObj); //replace the desired API name
+    console.log(apiResponse);
+  };
 
-          {/* Push Notification Options */}
-          <Text style={styles.headersText}>Select the notifications you would like to receive on your mobile device.</Text>
+  render() {
+    return (
+      <View style={styles.container}>
 
-          {/* Primary Device Notification */}
-          <View style={styles.backgroundScheduleBox}>
-            <Image style={styles.iconPictures} source={require('../assets/electric-car-icon.png')} />
-            <Text style={{...styles.optionText, paddingRight: 147}}>Primary Device</Text>
-            <Switch
-              style={styles.switch}
-              onValueChange={this.primaryDeviceToggle}
-              value={this.state.primaryDeviceSwitch} />
-          </View> 
+        {/* Push Notification Options */}
+        <Text style={styles.headersText}>Select the notifications you would like to receive on your mobile device.</Text>
 
-          {/* Secondary Device Notification */}
-          <View style={styles.backgroundScheduleBox}>
-            <Image style={styles.iconPictures} source={require('../assets/home-icon.png')} />
-            <Text style={{...styles.optionText, paddingRight: 120}}>Secondary Device</Text>
-            <Switch
-              style={styles.switch}
-              onValueChange={this.secondaryDeviceToggle}
-              value={this.state.secondaryDeviceSwitch} />
-          </View>
+        {/* Primary Device Notification */}
+        <View style={styles.backgroundScheduleBox}>
+          <Image style={styles.iconPictures} source={require('../assets/electric-car-icon.png')} />
+          <Text style={{ ...styles.optionText, paddingRight: 147 }}>Primary Device</Text>
+          <Switch
+            style={styles.switch}
+            onValueChange={this.primaryDeviceToggle}
+            value={this.state.primaryDeviceSwitch} />
+        </View>
 
-          {/* Interruptions Notification */}
-          <View style={styles.backgroundScheduleBox}>
-            <Image style={styles.iconPictures} source={require('../assets/pause-icon.png')} />
-            <Text style={{...styles.optionText, paddingRight: 100}}>Charge Interruptions</Text>
-            <Switch
-              style={styles.switch}
-              onValueChange={this.chargeInterruptToggle}
-              value={this.state.chargeInterruptSwitch} />
-          </View >
+        {/* Secondary Device Notification */}
+        <View style={styles.backgroundScheduleBox}>
+          <Image style={styles.iconPictures} source={require('../assets/home-icon.png')} />
+          <Text style={{ ...styles.optionText, paddingRight: 120 }}>Secondary Device</Text>
+          <Switch
+            style={styles.switch}
+            onValueChange={this.secondaryDeviceToggle}
+            value={this.state.secondaryDeviceSwitch} />
+        </View>
 
-          {/* <View style={styles.backgroundScheduleBox}>
+        {/* Interruptions Notification */}
+        <View style={styles.backgroundScheduleBox}>
+          <Image style={styles.iconPictures} source={require('../assets/pause-icon.png')} />
+          <Text style={{ ...styles.optionText, paddingRight: 100 }}>Charge Interruptions</Text>
+          <Switch
+            style={styles.switch}
+            onValueChange={this.chargeInterruptToggle}
+            value={this.state.chargeInterruptSwitch} />
+        </View >
+
+        {/* <View style={styles.backgroundScheduleBox}>
             <Image style={styles.iconPictures} source={require('../assets/pause-icon.png')} />
             <Text style={{...styles.optionText, paddingRight: 90}}>Remind me to plug in </Text>
             <Switch
@@ -100,75 +102,76 @@ class NotificationSelectionScreen extends React.Component {
               value={this.state.chargeInterruptSwitch} />
           </View > */}
 
-          <View style={styles.logoutContainer}>
-                <View style={styles.logoutButtonContainer}>
-                    <Button onPress={this.onlogInfo}
-                        title="Save"
-                        color='white'
-                    />
-            </View>
+        <View style={styles.logoutContainer}>
+          <View style={styles.logoutButtonContainer}>
+            <Button onPress={this.onlogInfo}
+              title="Save"
+              color='white'
+            />
+          </View>
         </View>
 
-          </View>
-        );
-    }
+      </View>
+    );
+  }
 }
 
 
 // for navigation to other screens
-const AppStackNavigator = createStackNavigator({
-  Schedule: { screen: NotificationSelectionScreen }});
-const Apps = createAppContainer(AppStackNavigator);
+// const AppStackNavigator = createStackNavigator({
+//   Schedule: { screen: NotificationSelectionScreen }
+// });
+// const Apps = createAppContainer(AppStackNavigator);
 
 // styling elements
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Colors.primary,
-      minWidth: '100%'
-    },
-    optionText: {
-      fontSize: 20,
-      color: Colors.secondary,
-      marginLeft: 30
-    },
-    headersText: {
-      fontSize: 14,
-      color: 'grey',
-      fontWeight: 'bold',
-      padding: 15
-    },
-    iconPictures: {
-      marginLeft: 10, 
-      width: 30, 
-      height: 30
-    },
-    backgroundScheduleBox: {
-      flex: 1,
-      flexDirection: 'row',
-      width: '100%',
-      maxHeight: 60,
-      backgroundColor: Colors.appleBlue,
-      alignItems: 'center',
-      marginRight: 90,
-      marginTop: 15
-    },
-    logoutContainer: {
-      flex: 1,
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    logoutButtonContainer: {
-      backgroundColor: Colors.appleBlue,
-      position: 'absolute',
-      justifyContent: 'center',
-      height: 40,
-      borderColor: '#51a0d5',
-      borderWidth: 1,
-      bottom: '20%',
-      width: '90%',
-      borderRadius: 10
-    }
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primary,
+    minWidth: '100%'
+  },
+  optionText: {
+    fontSize: 20,
+    color: Colors.secondary,
+    marginLeft: 30
+  },
+  headersText: {
+    fontSize: 14,
+    color: 'grey',
+    fontWeight: 'bold',
+    padding: 15
+  },
+  iconPictures: {
+    marginLeft: 10,
+    width: 30,
+    height: 30
+  },
+  backgroundScheduleBox: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%',
+    maxHeight: 60,
+    backgroundColor: Colors.appleBlue,
+    alignItems: 'center',
+    marginRight: 90,
+    marginTop: 15
+  },
+  logoutContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  logoutButtonContainer: {
+    backgroundColor: Colors.appleBlue,
+    position: 'absolute',
+    justifyContent: 'center',
+    height: 40,
+    borderColor: '#51a0d5',
+    borderWidth: 1,
+    bottom: '20%',
+    width: '90%',
+    borderRadius: 10
+  }
 });
-export default Apps;
+export default NotificationSelectionScreen;
