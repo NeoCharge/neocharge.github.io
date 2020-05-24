aa390f89f307aad8d7ad579119c01cc52d2ed59d
caseydaly <caseydaly31@gmail.com>
2020-05-24 13:00:49 -0700

    y scaling now dynamic in week graph

--- neocharge-app/components/WeekGraph.js
+++ neocharge-app/components/WeekGraph.js
@@ -15,6 +15,8 @@ export default class WeekGraph extends React.PureComponent {
         this.state = {
             primary: this.props.primary.map((value) => ({ value })),
             secondary: this.props.secondary.map((value) => ({ value })),
+            max: 0,
+            scale: []
         }
     }
 
@@ -22,6 +24,28 @@ export default class WeekGraph extends React.PureComponent {
         if (prevProps.primary !== this.props.primary || prevProps.secondary !== this.props.secondary) {
             this.setState({ primary: this.props.primary.map((value) => ({ value })), secondary: this.props.secondary.map((value) => ({ value })) });
         }
+    }
+
+    getScale(primary, secondary) {
+        var tempMax = 0;
+        primary.forEach(x => { if (x.value > tempMax) { tempMax = x.value } });
+        secondary.forEach(x => { if (x.value > tempMax) { tempMax = x.value } });
+
+        //get next highest multiple of 5
+        this.state.max = Math.ceil(tempMax / 5) * 5;
+        if (tempMax <= 10) {
+            return [0, 2, 4, 6, 8, 10];
+        } else {
+            var i;
+            var j = 0;
+            const increment = this.state.max / 5;
+            var l = [];
+            for (i = 0; i <= this.state.max; i += increment) {
+                l[j++] = i
+            }
+            return l;
+        }
+
     }
 
     getDayName(dayVal) {
@@ -110,12 +134,23 @@ export default class WeekGraph extends React.PureComponent {
         const CustomGrid = ({ x, y, data, ticks }) => (
             <G>
                 {// Horizontal grid
-                    ticks.map(function (tick, index) {
-                        if (index % 2 == 0) {
-                            return (<Line key={tick} x1={"0%"} x2={"100%"} y1={y(tick)} y2={y(tick)} stroke={"grey"} />);
+                    // ticks.map(function (tick, index) {
+                    //     if (index % 2 == 0) {
+                    //         return (<Line key={tick} x1={"0%"} x2={"100%"} y1={y(tick)} y2={y(tick)} stroke={"grey"} />);
+                    //     }
+                    // })
+
+                    this.state.scale.map(
+                        function(value) {
+                            console.log("here are ticks");
+                            console.log(ticks);
+                            return (<Line key={value} x1={"0%"} x2={"100%"} y1={y(value)} y2={y(value)} stroke={"grey"} />);
                         }
-                    })
+                    )
+                    
                 }
+
+
             </G>
         );
 
@@ -138,11 +173,14 @@ export default class WeekGraph extends React.PureComponent {
 
         )
 
+        this.state.scale = this.getScale(this.state.primary, this.state.secondary);
+        console.log("scale is... ");
+        console.log(this.state.scale);
 
 
 
 
-        const yAxis = [0, 2, 4, 6, 8, 10].map((value) => ({ value }));
+        const yAxis = this.state.scale.map((value) => ({ value }));
         const contentInset = { top: 30, bottom: 15 };
         const xAxisHeight = 30;
         const width = Dimensions.get('window').width
@@ -161,9 +199,10 @@ export default class WeekGraph extends React.PureComponent {
                         spacingInner={0.45}
                         spacingOuter={0.30}
                         gridMin={0}
+                        numberOfTicks={6}
                         //grid
                         // svg={{rx:"20"}} tried to make edges round but doesn't work
-                        yMax={10}
+                        yMax={this.state.max}
                     >
                         {/* <CustomBars bandwidth={10} /> */}
                         <CustomGrid belowChart={true} />
@@ -171,13 +210,16 @@ export default class WeekGraph extends React.PureComponent {
                         <Gradient2 />
                     </BarChart>
                     <YAxis
-                        data={yAxis}
+                        data={yAxis.reverse()}
+                        scale={scale.scaleBand}
                         yAccessor={({ item }) => item.value}
+                        spacingInner={1}
                         //style={{ marginBottom: xAxisHeight }}
                         contentInset={contentInset}
                         svg={{ fontSize: 10, fill: Colors.secondary, paddingHorizontal: 20 }}
                         formatLabel={(value) => value}
-                        numberOfTicks={5}
+                        numberOfTicks={6}
+                        yMax={this.state.max}
                     />
                 </View>
                 <View style={{ width: '100%', alignSelf: 'stretch', paddingRight:18}}>
